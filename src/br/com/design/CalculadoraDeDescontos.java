61feb5d219a35a997d1c7b48d5c5411df8c9a586
DESKTOP-J1UVOIK\edesio.schemes <edesio@4ward.com.br>
2017-12-04 13:00:23 -0200

    Atualizando

---
+++ src/br/com/design/CalculadoraDeDescontos.java
@@ -0,0 +1,5 @@
+package br.com.design;
+
+public class CalculadoraDeDescontos {
+
+}
--- src/br/com/design/strategy/Orcamento.java
+++ src/br/com/design/strategy/Orcamento.java
@@ -1,21 +1,31 @@
 package br.com.design.strategy;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import br.com.design.Item;
+
 public class Orcamento {
 
-	private double valor;
+	private final double valor;
+	private final List<Item> itens;
 
 	public Orcamento(double valor) {
 		this.valor = valor;
+		itens = new ArrayList<Item>();
 	}
 
 	public double getValor() {
 		return valor;
 	}
-
-	public void setValor(double valor) {
-		this.valor = valor;
-	}
-
 	
+	public void adicionaItem(Item item) {
+		itens.add(item);
+	}
+	
+	public List<Item> getItens() {
+		return Collections.unmodifiableList(itens);
+	}
 	
 }
